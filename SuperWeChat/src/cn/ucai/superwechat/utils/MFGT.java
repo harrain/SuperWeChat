f4fc98ca36a69bff68aad830c2b5cc63fbea20c7
harrain <renzhicheng00@163.com>
2017-05-19 15:34:49 +0800

    添加跳转工具类

---
+++ SuperWeChat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,29 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.MainActivity;
+
+/**
+ * Created by CDLX on 2017/5/19.
+ */
+
+public class MFGT {
+
+    private static void startActivity(Context context,Class clazz){
+        context.startActivity(new Intent(context,clazz));
+        ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+
+    public static void gotoMain(Activity activity){
+        startActivity(activity, MainActivity.class);
+    }
+
+    public static void gotoLogin(Activity activity){
+        startActivity(activity, LoginActivity.class);
+    }
+}
